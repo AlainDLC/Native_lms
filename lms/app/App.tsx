19808169adbed8361c44993ab55bf8c2f8dbd7dd
AlainDLC <jd_alain@hotmail.com>
2025-01-13 12:10:52 +0100

    clean up

--- lms/app/App.tsx
+++ lms/app/App.tsx
@@ -1,10 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-
 import "react-native-reanimated";
-
 import { Text, View } from "react-native";
 
-export default function RootLayout() {
+export default function App() {
   return (
     <View>
       <Text>Nu händer det</Text>
